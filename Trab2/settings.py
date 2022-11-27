a088ffbf06905830d1e23b43930237075f76423a
Thomas Martial <thomas.martial@insa-lyon.fr>
2022-11-27 17:59:36 -0300

    test deploy heroku

--- Trab2/settings.py
+++ Trab2/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,7 +120,13 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
-django_heroku.settings(locals())
+
+# Configure Django App for Heroku.
+try:
+    import django_heroku
+    django_heroku.settings(locals())
+except:
+    pass
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
